3194b8ce800ab0fe4320d1e6da5cb3c0108efa47
Legion <gurbindersingh8727@gmail.com>
2025-12-30 18:40:04 +0530

    feat: add feedback buttons for heat maps

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,9 +1,8 @@
 import { DocsTableOfContents } from "@/components/docs/mdx/components/table-of-content";
+import { FeedbackButtons } from "@/components/docs/mdx/components/feedback-buttons";
 import { MDXNavigation } from "@/components/docs/mdx/components/navigation";
-import { ThumbsDownIcon, ThumbsUpIcon } from "@/assets/icons";
 import { findNeighbour } from "fumadocs-core/page-tree";
 import { mdxComponents } from "@/components/docs/mdx";
-import { Button } from "@/components/ui/button";
 import { notFound } from "next/navigation";
 import { source } from "@/lib/source";
 
@@ -27,27 +26,19 @@ export default async function Page(props: { params: Promise<{ slug: string[] }> 
     <div className="relative flex">
       <div className="docs-container flex flex-col py-12 pb-32">
         <div className="flex flex-col gap-1">
-          <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight sm:text-3xl xl:text-4xl">{doc.title}</h1>
-          {doc.description && <p className="text-muted-foreground text-[15px]">{doc.description}</p>}
+          <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight sm:text-3xl xl:text-4xl">
+            {doc.title}
+          </h1>
+          {doc.description && (
+            <p className="text-muted-foreground text-[15px]">{doc.description}</p>
+          )}
         </div>
         <div className="text-primary/80 mt-8 w-full flex-1 text-[14px] *:data-[slot=alert]:first:mt-0">
           <MDX components={mdxComponents} />
         </div>
         <div className="mt-40 flex flex-col gap-8">
           <div className="flex flex-row items-center justify-between">
-            <div className="flex flex-row items-center gap-4">
-              <span className="text-muted-foreground text-sm">Did you like the content?</span>
-              <div className="space-x-2">
-                <Button className="text-muted-foreground hover:text-primary" variant="outline" size="sm">
-                  <ThumbsUpIcon />
-                  <span>Good</span>
-                </Button>
-                <Button className="text-muted-foreground hover:text-primary" variant="outline" size="sm">
-                  <ThumbsDownIcon />
-                  <span>Bad</span>
-                </Button>
-              </div>
-            </div>
+            <FeedbackButtons />
           </div>
           <div className="grid grid-cols-2 gap-8">
             <div>
