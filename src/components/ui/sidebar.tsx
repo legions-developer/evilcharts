68d5a495ca205be1800a14d77d1824d632bc3517
Legion <gurbindersingh8727@gmail.com>
2026-02-06 19:38:55 +0530

    chore: remove sidebar border

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -221,7 +221,7 @@ function Sidebar({
           // Adjust the padding for floating and inset variants.
           variant === "floating" || variant === "inset"
             ? "p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]"
-            : "border-border/80 group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r group-data-[side=right]:border-l",
+            : "group-data-[collapsible=icon]:w-(--sidebar-width-icon)",
           className,
         )}
         {...props}
