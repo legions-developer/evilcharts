2577900790d0306f0d809695aba345046d3faa66
Legion <gurbindersingh8727@gmail.com>
2026-03-10 13:52:44 +0530

    fix: improve transition duration for collapsible triggers in CodeCollapsibleWrapper component

--- src/components/docs/charts/code-collapsible-wrapper.tsx
+++ src/components/docs/charts/code-collapsible-wrapper.tsx
@@ -5,8 +5,8 @@ import {
   CollapsibleTrigger,
   CollapsiblePanel,
 } from "@/components/ui/base-collapsible";
-import { cn } from "@/lib/utils";
 import type { ComponentProps } from "react";
+import { cn } from "@/lib/utils";
 
 export function CodeCollapsibleWrapper({
   className,
@@ -22,12 +22,12 @@ export function CodeCollapsibleWrapper({
       >
         {children}
       </CollapsiblePanel>
-      <CollapsibleTrigger className="to-background text-muted-foreground hover:[&>div]:border-primary/50 hover:text-foreground absolute inset-x-0 -bottom-1 flex h-16 cursor-pointer items-end justify-end bg-linear-to-b from-transparent via-50% text-sm font-medium transition-colors group-data-open/collapsible:hidden">
-        <div className="bg-background mr-4 mb-4 rounded-md border px-2 py-0.5 font-normal shadow-[0px_0px_8px_20px_var(--color-background)]/50 duration-200">
+      <CollapsibleTrigger className="to-background text-muted-foreground hover:[&>div]:border-primary/50 hover:text-foreground absolute inset-x-0 -bottom-1 flex h-16 cursor-pointer items-end justify-end bg-linear-to-b from-transparent via-50% text-sm font-medium transition-colors duration-[0ms] group-data-open/collapsible:hidden hover:duration-200">
+        <div className="bg-background mr-4 mb-4 rounded-md border px-2 py-0.5 font-normal shadow-[0px_0px_8px_20px_var(--color-background)]/50">
           Expand
         </div>
       </CollapsibleTrigger>
-      <CollapsibleTrigger className="text-muted-foreground hover:border-primary/50 hover:text-foreground bg-background absolute right-4 bottom-4 hidden cursor-pointer rounded-md border px-2 py-0.5 text-sm font-normal shadow-sm transition-colors group-data-open/collapsible:block">
+      <CollapsibleTrigger className="text-muted-foreground hover:border-primary/50 hover:text-foreground bg-background absolute right-4 bottom-4 hidden cursor-pointer rounded-md border px-2 py-0.5 text-sm font-normal shadow-sm transition-colors duration-[0ms] group-data-open/collapsible:block hover:duration-200">
         Collapse
       </CollapsibleTrigger>
     </Collapsible>
