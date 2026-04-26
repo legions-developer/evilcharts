a617c1a1a33e29c375be3299e79b6867f46c772c
Legion <gurbindersingh8727@gmail.com>
2026-04-27 02:14:28 +0530

    chore: update recharts dependency to version 3.8.1 and enable inline CSS in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  experimental: {
+    inlineCss: true,
+  },
   async redirects() {
     return [
       {
